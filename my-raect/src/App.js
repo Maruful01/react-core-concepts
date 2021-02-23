f9f19d33c4e758d5df43dcfea8a4b53a938dcea2
Maruful <tonmoykhan46@yahoo.com>
2021-02-23 18:05:23 +0600

    props added

--- my-raect/src/App.js
+++ my-raect/src/App.js
@@ -3,39 +3,66 @@ import './App.css';
 import reactDom from 'react-dom';
 
 function App() {
-  const phone = {brand: "Samsung", model: "A 51", color: "Blue", prise: 27000}
-  const brandName = ['Samsung', 'Nokia', 'Oppo']
-  const style = {
-    color: 'yellow',
-    boxShadow: '5px 5px 10px black',
-    padding: '5px'
-  }
+
+  const brandName = ['Samsung', 'Nokia', 'Oppo'];
+  const products = [
+                     {name: 'Photoshop', price: '$100'},
+                     {name: 'Illustrator', price: '$80.90'}
+                    ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>{phone.brand}</h3>
-        <h4>{phone.model}</h4>
-        <p style={{color: 'blue', backgroundColor: "white"}}>{phone.prise}</p>
-        <p style= {style}>{phone.color}</p>
-        <Product brand= {brandName[0]} model="A 51" prise="100 $" color="Blue"></Product>
-        <Product brand= {brandName[1]} model="Lumia" prise="150 $" color="Red"></Product>
-        <Product brand= {brandName[2]} model="F 17" prise="80 $" color="Pink"></Product>
+        <Product brand= {brandName[0]} model="A 51" price="100 $" color="Blue"></Product>
+        <Product brand= {brandName[1]} model="Lumia" price="150 $" color="Red"></Product>
+        <Product brand= {brandName[2]} model="F 17" price="80 $" color="Pink"></Product>
+        <div style={{marginTop: '50px', width: '60%'}}>
+        <h1>Adobe Product</h1>
+        <div style={{marginTop: '20px', display: 'grid',
+                     gridTemplateColumns: '1fr 1fr 1fr',
+                   }}>
+        <AdobeProduct product={products[0]}> </AdobeProduct>
+        <AdobeProduct product={products[1]}> </AdobeProduct>
+        </div>
+        
+        </div>
+        
+
       </header>
     </div>
   );
 }
 function Product (props) {
-  console.log (props);
-  return (<div style  ={{
-                         backgroundColor: 'blue', 
+  const productStyle = { 
+                         backgroundColor: 'lightgray', 
                          boxShadow: '5px 5px 10px black',
                          width:'300px',
-                         margin: '10px'
-                      }}>
+                         margin: '10px',
+                         float: 'left'
+                       }
+  return (<div style  = {productStyle}>
     <h3> {props.brand} </h3>
     <h4> {props.model} </h4>
-    <p> {props.prise} <br/> <span> Color: {props.color} </span></p>
+    <p> {props.price} <br/> <span> Color: {props.color} </span></p>
+  </div>)
+}
+//
+function AdobeProduct (props) {
+  const productStyle = { 
+    backgroundColor: 'white', 
+    color: 'black',
+    boxShadow: '5px 5px 10px black',
+    width:'200px',
+    height: '200px',
+    margin: '10px',
+    border: '1px solid gray',
+    borderRadius: '10px'
+  }
+  const {name, price} = props.product;
+  console.log (props.product.price);
+  return (<div style  = {productStyle}>
+    <h3>{name}</h3>
+    <h5>{price}</h5>
+    <button>Buy now</button>
   </div>)
 }
 
